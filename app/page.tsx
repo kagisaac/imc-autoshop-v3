f21831fa9d793132901489c9d6190faf18bb435f
kagenza isaac <kagisaac103@gmail.com>
2024-03-28 16:54:22 +0200

    add responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <>
     <NavBar/>
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-2 pt-20">
      <Hero/>
      <CarDemo />
     </main>
